71ff18d8e4044337ac560dfc89095ae48e059938
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-12-30 05:26:46 -0500

    task status implementation done

--- src/components/CompletedTask/CompletedTask.js
+++ src/components/CompletedTask/CompletedTask.js
@@ -1,18 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const CompletedTask = () => {
+	const {user}=useContext(AuthContext)
+	const {data: tasks = [], refetch} = useQuery({
+        queryKey: ['user'],
+        queryFn: async() =>{
+            const res = await fetch(`https://tasks-noyonahammadkhan.vercel.app/completedtasks/${user?.email}`);
+            const data = await res.json();
+            return data;
+        }
+    });
 	const handleCompleteStatus=(id)=>{
-		fetch(`https://tasks-nine.vercel.app//tasks/completed/${id}`,{
+		fetch(`https://tasks-noyonahammadkhan.vercel.app/tasks/completed/${id}`,{
 			method:'PATCH',
 			headers:{
 				'content-type':'application/json'
 			},
 			body:JSON.stringify({completeStatus:'notcompleted'})
 		})
+		.then(res=>res.json())
+		.then(data=>{
+			console.log(data);
+			toast.success('task status notcompleted done')
+			refetch()
+		})
 	}
     return (
         <div>
-            <h1>Completed Task</h1>
+            <h1>Completed Task{tasks.length}</h1>
             <div className="container w-6/12 p-2 mx-auto sm:p-4 dark:text-gray-100">
 	<h2 className="mb-4 text-2xl font-semibold leading-tight">Invoices</h2>
 	<div className="overflow-x-auto">
@@ -37,12 +56,13 @@ const CompletedTask = () => {
 				</tr>
 			</thead>
 			<tbody>
-				<tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+				{
+					tasks.map((task,i)=><tr key={task._id} className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
 					<td className="p-3">
 						<p>1</p>
 					</td>
 					<td className="p-3">
-						<p>Microsoft Corporation</p>
+						<p>{task.taskName}</p>
 					</td>
 					<td className="p-3">
 						<p>14 Jan 2022</p>
@@ -53,13 +73,14 @@ const CompletedTask = () => {
 						<p className="dark:text-gray-400">Tuesday</p>
 					</td>
 					<td className="p-3">
-                    <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5">Not Completed</button>
+                    <button onClick={()=>handleCompleteStatus(task._id)} type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5">Not Completed</button>
 					</td>
 					
                     <td className="p-3">
                     <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete Task</button>
 					</td>
-				</tr>
+				</tr>)
+				}
 				
 			</tbody>
 		</table>
--- src/components/MyTask/MyTask.js
+++ src/components/MyTask/MyTask.js
@@ -1,18 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const MyTask = () => {
+	const {user}=useContext(AuthContext)
+	const {data: tasks = [], refetch} = useQuery({
+        queryKey: ['user'],
+        queryFn: async() =>{
+            const res = await fetch(`https://tasks-noyonahammadkhan.vercel.app/mytasks/${user?.email}`);
+            const data = await res.json();
+            return data;
+        }
+    });
+
 	const handleCompleteStatus=(id)=>{
-		fetch(`https://tasks-nine.vercel.app//tasks/completed/${id}`,{
+		fetch(`https://tasks-noyonahammadkhan.vercel.app/tasks/completed/${id}`,{
 			method:'PATCH',
 			headers:{
 				'content-type':'application/json'
 			},
 			body:JSON.stringify({completeStatus:'completed'})
 		})
+		.then(res=>res.json())
+		.then(data=>{
+			console.log(data);
+			toast.success('task completed')
+			refetch()
+		})
 	}
     return (
         <div>
-            <h1>My Task</h1>
+            <h1>My Task{tasks.length}</h1>
             <div className="container w-7/12 p-2 mx-auto sm:p-4 dark:text-gray-100">
 	<h2 className="mb-4 text-2xl font-semibold leading-tight">Invoices</h2>
 	<div className="overflow-x-auto">
@@ -37,12 +57,14 @@ const MyTask = () => {
 				</tr>
 			</thead>
 			<tbody>
-				<tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+				{
+					tasks.map((task,i)=><tr key={task._id} className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+						{console.log(task)}
 					<td className="p-3">
-						<p>1</p>
+						<p>{i+1}</p>
 					</td>
 					<td className="p-3">
-						<p>Microsoft Corporation</p>
+						<p>{task.taskName}</p>
 					</td>
 					<td className="p-3">
 						<p>14 Jan 2022</p>
@@ -53,7 +75,7 @@ const MyTask = () => {
 						<p className="dark:text-gray-400">Tuesday</p>
 					</td>
 					<td className="p-3">
-                    <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5">Completed</button>
+                    <button  onClick={()=>handleCompleteStatus(task._id)}  type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5">Completed</button>
 					</td>
 					<td className="p-3">
                     <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Update</button>
@@ -61,7 +83,9 @@ const MyTask = () => {
                     <td className="p-3">
                     <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
 					</td>
-				</tr>
+				</tr>)
+				}
+				
 				
 			</tbody>
 		</table>
