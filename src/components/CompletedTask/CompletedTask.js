f1dea7b75ae9e6d7316fab5aaef247cb641bc3a2
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-12-30 05:35:33 -0500

    delete functionality add

--- src/components/CompletedTask/CompletedTask.js
+++ src/components/CompletedTask/CompletedTask.js
@@ -29,6 +29,24 @@ const CompletedTask = () => {
 			refetch()
 		})
 	}
+
+
+	const handleDeleteTask = id => {
+        fetch(`https://tasks-noyonahammadkhan.vercel.app/tasks/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                refetch();
+                toast.success(`Task deleted successfully`)
+            }
+        })
+    }
+
+
+
+
     return (
         <div>
             <h1>Completed Task{tasks.length}</h1>
@@ -77,7 +95,7 @@ const CompletedTask = () => {
 					</td>
 					
                     <td className="p-3">
-                    <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete Task</button>
+                    <button onClick={()=>handleDeleteTask(task._id)} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete Task</button>
 					</td>
 				</tr>)
 				}
